3300013a1dfaa5fef9055744cd7534c588667702
Gjc452 <1796444035@qq.com>
2021-01-19 20:29:25 +0800

    ‘用 useReducer 代替 redux’

--- src/index.js
+++ src/index.js
@@ -1,17 +1,120 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useReducer, useContext, useEffect} from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.querySelector('#root')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = {
+  user: null,
+  books: null,
+  movies: null
+}
+
+function reducer(state, action) {
+  if (action.type === 'setUser') {
+    return {...state, user: action.user}
+  } else if (action.type === 'setBooks') {
+    return {...state, books: action.books}
+  } else if (action.type === 'setMovies') {
+    return {...state, movies: action.movies}
+  } else {
+    console.error('type error')
+  }
+}
+
+const Context = React.createContext(null)
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, store)
+  const api = {state, dispatch}
+  return (
+    <Context.Provider value={api}>
+      <User/>
+      <hr/>
+      <Books/>
+      <Movies/>
+    </Context.Provider>
+  )
+}
+
+const User = () => {
+  const {state, dispatch} = useContext(Context)
+  useEffect(() => {
+    ajax('/user').then(user => {
+      dispatch({type: 'setUser', user})
+    })
+  }, [])
+  return (
+    <div>
+      <h1>个人信息</h1>
+      <div>name: {state.user ? state.user.name : ""}</div>
+      {console.log(state.user)}
+    </div>
+  )
+}
+
+const Books = () => {
+  const {state, dispatch} = useContext(Context)
+  useEffect(() => {
+    ajax('/books').then(books => {
+      dispatch({type: 'setBooks', books})
+    })
+  }, [])
+  return (
+    <div>
+      <h1>我的书籍</h1>
+      <ol>{state.books
+        ? state.books.map(book => <li key={book.id}>{book.name}</li>) : '加载中'}</ol>
+    </div>
+  )
+}
+
+const Movies = () => {
+  const {state, dispatch} = useContext(Context)
+  useEffect(() => {
+    ajax('/movies').then(movies => {
+      dispatch({type: 'setMovies', movies})
+    })
+  }, [])
+  return (
+    <div>
+      <h1>我的电影</h1>
+      <ol>{state.movies
+        ? state.movies.map(movie => <li key={movie.id}>{movie.name}</li>) : '加载中'}</ol>
+    </div>
+  )
+}
+
+const ajax = (path) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (path === '/user') {
+        resolve({
+          id: 1,
+          name: 'Jack'
+        })
+      } else if (path === '/books') {
+        resolve([{
+          id: 1,
+          name: 'JavaScript 高级程序设计'
+        }, {
+          id: 2,
+          name: 'JavaScript 权威指南'
+        }])
+      } else if (path === '/movies') {
+        resolve([{
+          id: 1,
+          name: '头文字D'
+        }, {
+          id: 2,
+          name: '星球大战'
+        }])
+      }
+    }, 2000)
+  })
+
+
+}
+
+
+ReactDOM.render(<App/>, rootElement)
+
